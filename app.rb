1d2f1c0ed5b34fec4a4f35ef03b765a3cbb17df1
Juliette Gramatges <juliettegramatges@MacBook-Air-de-Juliette.local>
2025-06-18 21:24:04 -0400

    primer commit de bitácora

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'roo'
+
+set :public_folder, 'public'
+
+def cargar_desde_excel(tipo)
+  ruta = "./data/#{tipo}.xlsx"
+  xlsx = Roo::Excelx.new(ruta)
+
+  case tipo
+  when "aplausos"
+    xlsx.drop(1).map do |row|
+      {
+        nombre: row[0],
+        descripcion: row[1],
+        esquema: row[2]
+      }
+    end
+  when "dinamicas"
+    xlsx.drop(1).map do |row|
+      {
+        nombre: row[0],
+        descripcion: row[1],
+        esquema: row[2]
+      }
+    end
+  when "juegos"
+    xlsx.drop(1).map do |row|
+      {
+        nombre: row[0],
+        objetivo: row[1]
+      }
+    end
+  else
+    []
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/bitacora/:tipo' do
+  @tipo = params[:tipo]
+  @items = cargar_desde_excel(@tipo)
+  erb :bitacora
+end
